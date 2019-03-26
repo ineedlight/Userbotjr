a51fd1462d6ad822373de1072ab1742fb054b231
GABBARBOU <47245693+GABBARBOU@users.noreply.github.com>
2019-03-26 17:37:02 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -101,9 +101,10 @@ async def amireallyalive(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit(
             "`"
-            "Sar! I am alive ":D
-
-
-
+            "Sar! I am alive :D"
             "`"
             )
+
+
+
+
